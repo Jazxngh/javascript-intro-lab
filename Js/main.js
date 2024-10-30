66268f2bcc05d047c8b611d8fd8b031f9b0f5815
Jazxngh <b123456@my.shu.ac.uk>
2024-10-30 15:39:32 +0000

    Java-Script

---
+++ Js/main.js
@@ -0,0 +1,41 @@
+console.info("Hello from the file");
+document.getElementById("myHeading").innerHTML = "Jazz Singh";
+document.querySelector('nav ul li').setAttribute("class", "currentPage");
+//document
+//document.querySelector("nav ul li a")
+//.setAttribute("href", "https://www.google.co.uk/")
+
+let colourButtons = document.querySelectorAll(".colpicker");
+console.dir(colourButtons);
+
+for(let i=0; i<colourButtons.length; i++){
+    colourButtons[i].style.background.Color = "LightGrey";
+}
+
+document.getElementById("myTestBtn").addEventListener("click", function() { ("Hi I was clicked"); });
+
+let redBtn = document.querySelector(".red"); 
+redBtn.addEventListener("click", function() {
+    document.body.setAttribute("class","redBack");
+})
+
+let greenBtn = document.querySelector(".green");
+greenBtn.addEventListener("click", function() {
+    document.body.setAttribute("class", "greenBack");
+
+})
+
+let blueBtn = document.querySelector(".blue");
+blueBtn.addEventListener("click", function() {
+    document.body.setAttribute("class", "blueBack");
+
+})
+
+// The colour is based of a class in html, there can be multiple classes, so because in html
+// it says "green colPicker" with a space, its a seperate class so, .green would work
+
+let defaultBtn = document.querySelector(".default");
+defaultBtn.addEventListener("click", function() {
+    document.body.removeAttribute("class");
+
+})
